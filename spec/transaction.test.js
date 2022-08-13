b773189b5fc1eac35b853fa12d8dc0b5cd0f6607
George Dainton <gcdainton94@gmail.com>
2022-08-13 16:42:02 +0100

    Transaction balance added

--- spec/transaction.test.js
+++ spec/transaction.test.js
@@ -2,9 +2,15 @@ const Transaction = require('../lib/transaction')
 
 describe ('Transaction', () => {
 
-  it('Creates a record of a transaction', () => {
-    const transaction = new Transaction(100);
+  it('Initializes with an amount', () => {
+    const transaction = new Transaction(100, 100);
 
     expect(transaction.amount).toEqual(100)
+  })
+
+  it('Initializes with an amount', () => {
+    const transaction = new Transaction(100, 100);
+
+    expect(transaction.balance).toEqual(100)
   })
 })
