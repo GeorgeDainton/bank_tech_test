d54cb0e36df5b13d46bf295369083bae812ede40
George Dainton <gcdainton94@gmail.com>
2022-08-13 18:23:52 +0100

    transaction class  injected into bankAccount class

--- spec/bankAccount.test.js
+++ spec/bankAccount.test.js
@@ -1,15 +1,16 @@
 const BankAccount = require('../lib/bankAccount')
+const Transaction = require('../lib/transaction')
 
 describe('Bank Account', () => {
 
-  it('Depositing money increases balance', () => {
+  it('Increases balance depositing money', () => {
     const bankAccount = new BankAccount()
     bankAccount.deposit(100)
     
     expect(bankAccount.balance).toEqual(100)
   })
 
-  it('Withdrawing money decreases balance', () => {
+  it('Decreases balance withdrawing money ', () => {
     const bankAccount = new BankAccount()
     bankAccount.deposit(100)
     bankAccount.withdraw(50)
@@ -17,9 +18,25 @@ describe('Bank Account', () => {
     expect(bankAccount.balance).toEqual(50)
   })
 
-  it('Withdrawing below 0 throws an error', () => {
+  it('Throws an error withdrawing below 0', () => {
     const bankAccount = new BankAccount()
    
     expect(() => { bankAccount.withdraw(1) }).toThrow('Error! Insufficient funds!');
   })
+
+  it('Creates a new transaction when depositing money', () => {
+    const bankAccount = new BankAccount(transaction = Transaction)
+    bankAccount.deposit(100)
+
+    expect(bankAccount.transactions[0]).toEqual(
+      {
+        amount: 100,
+        balance: 100,
+        credit: true,
+        date: new Date()
+      }
+      )
+
+  })
+
 })
