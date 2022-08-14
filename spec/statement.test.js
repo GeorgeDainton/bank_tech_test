19051ed5e676cc6db8d05b3f349a734dcccc1fe2
George Dainton <gcdainton94@gmail.com>
2022-08-14 14:36:43 +0100

    Statement class displays transactions

--- spec/statement.test.js
+++ spec/statement.test.js
@@ -10,25 +10,30 @@ describe ('Statement', () => {
   })
 
   it('Contains transaction date', () => {
-    const statement = new Statement(transaction = new Transaction(100, 100, true, new Date().toLocaleDateString()));
+    const statement = new Statement
+    const transaction = new Transaction(100, 100, true);
     statement.transactions.push(transaction)
     
     expect(statement.display()).toContain(new Date().toLocaleDateString())
   })
 
   it('Contains credit amount', () => {
-    const statement = new Statement(transaction = new Transaction(100, 100, true, new Date().toLocaleDateString()));
+    const statement = new Statement
+    const transaction = new Transaction(100, 100, true);
     statement.transactions.push(transaction)
     
     expect(statement.display()).toContain('100')
   })
   
-  // it('Contains debit amount', () => {
-  //   const statement = new Statement(transaction = new Transaction(100, 100, true, new Date().toLocaleDateString()));
-  //   statement.transactions.push(transaction)
+  it('Contains debit amount', () => {
+    const statement = new Statement
+    const transaction1 = new Transaction(100, 100, true);
+    const transaction2 = new Transaction(50, 50, false);
+    statement.transactions.push(transaction1)
+    statement.transactions.push(transaction2)
     
-  //   expect(statement.display()).toContain(new Date().toLocaleDateString())
-  // })
+    expect(statement.display()).toContain('50')
+  })
   
   // it('Contains resulting balance after transaction', () => {
   //   const statement = new Statement(transaction = new Transaction(100, 100, true, new Date().toLocaleDateString()));
@@ -37,4 +42,4 @@ describe ('Statement', () => {
   //   expect(statement.display()).toContain(new Date().toLocaleDateString())
   // })
 
-})
+  })
